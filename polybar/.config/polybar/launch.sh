167f51cb46bfeb154659f854b2622d41561f26f4
DJ Coombes <contact@djcoombes.com>
2021-06-02 21:00:08 +0100

    Fixed polybar launching. Changed look of i3 in polybar. Added missing rofi theme.

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -7,4 +7,4 @@ killall -q polybar
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log
-polybar main 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar main --config=~/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar1.log & disown
